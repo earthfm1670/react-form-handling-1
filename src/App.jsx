7d3fc74084cffeeba4548c93f7dec114219376bf
Werapol Jaranai <r5r5wa55@hotmail.com>
2024-10-23 14:29:48 +0700

    feat:add featuer greeing Message

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,28 @@
+import { useState } from "react";
 import "./App.css";
 
 function App() {
+  let [greetingMes, setGreetingMes] = useState("Greeting Message");
+  let [textGreetingMes, setTextGreetingMes] = useState("");
+  console.log(textGreetingMes);
+  const handleConfilm = () => {
+    setGreetingMes(textGreetingMes ? textGreetingMes : "Greeting Message");
+  };
+
   return (
     <div className="App">
-      <div className="greeting-container">Greeting Message</div>
+      <div className="greeting-container">{greetingMes}</div>
       <div className="input-container">
         <label htmlFor="greeting-message">New Greeting Message</label>
-        <input id="greeting-message" type="text" />
+        <input
+          id="greeting-message"
+          type="text"
+          onChange={(e) => setTextGreetingMes(e.target.value)}
+        />
       </div>
 
       <div className="buttons">
-        <button>Update text</button>
+        <button onClick={handleConfilm}>Update text</button>
       </div>
     </div>
   );
